a4a034778061f22906322013c2f5e698c026d63f
“Nick <amellocello@msn.com>
2018-06-26 22:46:29 -0700

    Add readme, update form with error check.

---
+++ README.md
@@ -0,0 +1,43 @@
+# _New Project_
+
+#### _A Website that allows for quick creation of new projects, June 26, 2018_
+
+#### By _**Nick Rogers and Colin Bates**_
+
+## Description
+
+_This website was created to quickly create new projects in javascript and csharp including pairs and README's.
+Specs:
+1. User inputs information into a form and chooses desired template
+  Example input: project name: NewProject
+  Example output: (fields populate where needed)
+2. Steps are laid out for user to copy.
+  Example input: -form submit-
+  Example output: Step 1: copy this, Step 2: copy that
+3. Throw alerts when template will break.
+  Example input: -no name entered-
+  Example output: alert! no name entered.
+_
+
+## Setup/Installation Requirements
+
+* _Double click the icon in your folder_
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and contact details
+
+_Nick Rogers email: amellocello@msn.com_
+_Colin Bates email: dragonflylord@gmail.com_
+
+## Technologies Used
+
+_Created with Atom editor.  Use any browser to view._
+
+### License
+
+*No license required*
+
+Copyright (c) 2018 **_Nick Rogers and Colin Bates_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,93 @@
+function initials(name){
+  var nameToArray = name.split(" ");
+  return nameToArray[0].charAt(0).toLowerCase() + nameToArray[1].charAt(0).toLowerCase();
+}
+function nameCheck(name){
+  var nameToArray = name.split(" ");
+  return 2 !== nameToArray.length;
+}
+
 $(document).ready(function(){
   $("#form-1").submit(function(event){
     event.preventDefault();
+    //Take in values
     var inputtedProjectName = $("input#project-name").val();
-    var inputtedPartner1Initials = $("input#partner1-initials").val();
-    var inputtedPartner2Initials = $("input#partner2-initials").val();
+    var inputtedPartner1Name = $("input#partner1-name").val();
+    var inputtedPartner2Name = $("input#partner2-name").val();
+    var inputtedPartner1Email = $("input#partner1-email").val();
+    var inputtedPartner2Email = $("input#partner2-email").val();
+    var inputtedTemplate = $("input:radio[name=template]:checked").val();
 
+    //Empty fields/broken input warnings
+    if(inputtedProjectName === "" && inputtedTemplate !== "pairs") {
+      alert("your project name is blank btw.")
+    }
+    if(inputtedPartner1Name === "" || inputtedPartner2Name === ""){
+      alert("your partner initials won't be there unless you enter your names.")
+    } else if (nameCheck(inputtedPartner1Name)){
+      alert("partner 1 name will break my program >_> Make sure there's a first name and a last name with no space on the end")
+    } else if (nameCheck(inputtedPartner2Name)){
+      alert("partner 2 name will break my program <_< Make sure there's a first name and a last name with no space on the end")
+    } else {
+      var partner1Initials = initials(inputtedPartner1Name);
+      var partner2Initials = initials(inputtedPartner2Name);
+      if (partner1Initials === partner2Initials) {
+        alert("ya'll have the same initials. you can't git pair with the same initials. i'll let you figure that out.")
+      }
+    }
+    if (inputtedTemplate === "pairs" && (inputtedPartner1Email === "" || inputtedPartner2Email === "")) {
+      alert("nice email :thinking:");
+    }
+    //Populate fields
     $(".project-name").text(inputtedProjectName);
-    $(".partner1").text(inputtedPartner1Initials);
-    $(".partner2").text(inputtedPartner2Initials);
+    $(".partner1-fullname").text(inputtedPartner1Name);
+    $(".partner2-fullname").text(inputtedPartner2Name);
+    $(".partner-email1").text(inputtedPartner1Email);
+    $(".partner-email2").text(inputtedPartner2Email);
+    $(".partner1").text(partner1Initials);
+    $(".partner2").text(partner2Initials);
+
+    //Hide/show
+    if(inputtedTemplate === "pairs") {
+      $(".pairs-template").fadeIn();
+      $(".javascript").hide();
+      $(".CSharp").hide();
+    } else if (inputtedTemplate === "javascript") {
+      $(".pairs-template").hide();
+      $(".javascript").fadeIn();
+      $(".CSharp").hide();
+    } else if (inputtedTemplate === "csharp") {
+      $(".pairs-template").hide();
+      $(".javascript").hide();
+      $(".CSharp").fadeIn();
+    } else {
+      alert("Idk how you managed to get this option...");
+    }
+    // //Create click functions
+    // $(".pairs-click").click(function(){
+    //   $(".pairs-template").fadeIn();
+    //   $(".javascript").hide();
+    //   $(".CSharp").hide();
+    //   if (inputtedPartner1Email === "" || inputtedPartner2Email === "") {
+    //     alert("You need an email entered for .pairs")
+    //   }
+    // });
+    // $(".javascript-click").click(function(){
+    //   $(".pairs-template").hide();
+    //   $(".javascript").fadeIn();
+    //   $(".CSharp").hide();
+    //   if (inputtedProjectName === "") {
+    //     alert("Make sure you enter a project name");
+    //   }
+    // });
+    // $(".csharp-click").click(function(){
+    //   $(".pairs-template").hide();
+    //   $(".javascript").hide();
+    //   $(".CSharp").fadeIn();
+    //   if (inputtedProjectName === "") {
+    //     alert("Make sure you enter a project name");
+    //   }
+    // });
+
   });
 });
